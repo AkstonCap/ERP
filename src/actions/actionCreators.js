7b82459c111a46a4ba109acb50164a4e7af46b4c
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2025-03-02 20:52:31 +0100

    Update actionCreators.js

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -1,14 +1,11 @@
 import * as TYPE from './types';
 
-export const showConnections = () => ({
-  type: TYPE.SHOW_CONNECTIONS,
-});
-
-export const hideConnections = () => ({
-  type: TYPE.HIDE_CONNECTIONS,
-});
-
 export const updateInput = (inputValue) => ({
   type: TYPE.UPDATE_INPUT,
   payload: inputValue,
+});
+
+export const switchTab = (tab) => ({
+  type: TYPE.SWITCH_TAB,
+  payload: tab,
 });

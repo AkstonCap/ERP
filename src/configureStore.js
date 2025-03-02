0cb5ec2224ee8cc12aa4814f57b5e366b67af0ac
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2025-03-02 20:53:10 +0100

    Update configureStore.js

--- src/configureStore.js
+++ src/configureStore.js
@@ -7,7 +7,7 @@ export default function configureStore() {
   //Middlewares will automatically save when the state as changed,
   //ie state.settings will be stored on disk and will save every time state.settings is changed.
   const middlewares = [
-    storageMiddleware(({ settings }) => ({ settings })), //Data saved to disk
+    //storageMiddleware(({ settings }) => ({ settings })), //Data saved to disk
     stateMiddleware(({ ui }) => ({ ui })), //Data saved to session
   ];
   const enhancers = [applyMiddleware(...middlewares)];
